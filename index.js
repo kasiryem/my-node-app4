75954ad6c09265ba72ad9a690cfc5987fe3624d8
kasiryem <kasiryemoses1@gmail.com>
2024-06-30 00:34:07 +0300

    Final Modification of the CICD pipeline

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World welcome to the deployment class');
+  res.send('Hello, World! welcome to the deployment Master class. One of the best classes i have ever heard');
 });
 
 app.listen(PORT, () => {
